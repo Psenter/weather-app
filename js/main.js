d429ff983e0147b07a314700e654335f85d223a1
Parker S <111610335+Psenter@users.noreply.github.com>
2023-06-15 17:49:25 -0400

    Added notes to JS.

--- js/main.js
+++ js/main.js
@@ -9,62 +9,88 @@ let API_PATH = "/data/2.5/weather";
 let page = document.getElementById("page");
 
 //when the page loads it runs renderUI
-window.addEventListener('load', renderUI);
+window.addEventListener("load", renderUI);
 
 //RENDERS the elements onto the screen
 function renderUI() {
-    //adds the 'Weather App' title to the page
-    let h1 = document.createElement('h1');
-    h1.innerHTML = 'Weather App';
-    page.appendChild(h1);
+  //adds the 'Weather App' title to the page
+  let h1 = document.createElement("h1");
+  h1.innerHTML = "Weather App";
+  page.appendChild(h1);
 
-    //adds a user input box for the person to type their chosen zipcode into
-    let userInput = document.createElement('input');
-    userInput.setAttribute('placeholder', 'Enter Zipcode.');
-    userInput.setAttribute('id', 'userZipCodeInput')
-    page.appendChild(userInput);
+  //adds a user input box for the person to type their chosen zipcode into
+  let userInput = document.createElement("input");
+  userInput.setAttribute("placeholder", "Enter Zipcode.");
+  userInput.setAttribute("id", "userZipCodeInput");
+  page.appendChild(userInput);
 
-    //adds a button to start the search for weather in the area the user wants
-    let btn = document.createElement('button');
-    btn.innerHTML = 'Get Weather';
-    page.appendChild(btn);
-    btn.addEventListener('click', function() {
-        return getweatherData();
-    });
+  //adds a button to start the search for weather in the area the user wants
+  let btn = document.createElement("button");
+  btn.innerHTML = "Get Weather";
+  page.appendChild(btn);
+  btn.addEventListener("click", function () {
+    return getweatherData();
+  });
 
-    let weatherTables = document.createElement('div');
-    weatherTables.setAttribute('id', 'weatherData');
-    page.appendChild(weatherTables);
+  //creates a div with an id of 'weatherData' and puts it onto the screen
+  let weatherTables = document.createElement("div");
+  weatherTables.setAttribute("id", "weatherData");
+  page.appendChild(weatherTables);
 }
 
 function getweatherData() {
-    //uses axios to make a call to api
-    //userZipCode is a var that saves the user input and puts it into the API
-    //API_KEY is a global var used
-    let userZipCode = document.getElementById('userZipCodeInput').value;
-    axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=${userZipCode}&appid=${API_KEY}`)
-        .then(response => {
-            let data = response.data;
-            console.log(data);
-            let weatherInfo = createWeatherTables(data);
-            let weatherDataDiv = document.getElementById("weatherData");
-            weatherDataDiv.innerHTML = weatherInfo;
-        })
-        //IF an error happens, an alert is displayed saying there was an error 
-        .catch (error => {
-            console.error(error);
-            alert("There was an issue getting weather data.\nPlease check your zipcode.");
-        });
+  //uses axios to make a call to api
+  //userZipCode is a var that saves the user input and puts it into the API
+  //API_KEY is a global var used
+  let userZipCode = document.getElementById("userZipCodeInput").value;
+  axios
+    .get(
+      `http://api.openweathermap.org/data/2.5/weather?zip=${userZipCode}&appid=${API_KEY}`
+    )
+
+    //.then statment is what to do if there is data to gather
+    .then((response) => {
+      //stores the API call in var data
+      let data = response.data;
+
+      //logs all the data in console
+      console.log(data);
+
+      //calls the below function and sets the var equal to the function
+      let weatherInfo = createWeatherTables(data);
+
+      //gets the created empty div from 'renderUI' function
+      let weatherDataDiv = document.getElementById("weatherData");
+
+      //sets the div in the 'renderUI' function equal to the weatherInfo(which is set equal to the function)
+      weatherDataDiv.innerHTML = weatherInfo;
+    })
+
+    //IF an error happens, an alert is displayed saying there was an error
+    .catch((error) => {
+      console.error(error);
+      alert(
+        "There was an issue getting weather data.\nPlease check your zipcode."
+      );
+    });
 }
 
 function createWeatherTables(data) {
-    let weatherIcon = data.weather[0].icon;
-    let iconUrl = `https://openweathermap.org/img/wn/${weatherIcon}.png`;
-    let tempK = data.main.temp;
-    let tempC = (tempK - 273.15).toFixed(1);
-    let tempF = ((tempC * 9/5) + 32).toFixed(1);
-    let weatherZipDataDisplay = 
-    `
+  //gets the image ID
+  //then is inserted into URL to complete it and make it able to be pulled into the html
+  let weatherImage = data.weather.icon;
+  let weatherImageUrl = `https://openweathermap.org/img/wn/${weatherImage}.png`;
+
+  //gets the temp from the API
+  //converts temp kelvin into celsius and fahrenheit
+  let tempK = data.main.temp;
+  let tempC = (tempK - 273.15).toFixed(1);
+  let tempF = ((tempC * 9) / 5 + 32).toFixed(1);
+
+  //this is a template literal
+  //allows me to make a multi-line string
+  //also allows me to insert temps and other data directly into the string
+  let weatherZipDataDisplay = `
       <div class="container">
         <div class="row">
             <div class="col">
@@ -151,10 +177,10 @@ function createWeatherTables(data) {
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <img src="${iconUrl}" alt="Weather picture.">
+                    <img src="${weatherImageUrl}" alt="Weather picture.">
                 <div>
             </div>
         </div>
     `;
-    return weatherZipDataDisplay;
+  return weatherZipDataDisplay;
 }
