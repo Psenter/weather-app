60dece5258f47963b7a97847bc9597e9417e2b89
Parker S <111610335+Psenter@users.noreply.github.com>
2023-10-02 13:12:23 -0400

    Added more info, added some styling.

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ page.insertAdjacentHTML("beforeend", `
     <div class="row justify-content-center mt-5">
       <input class="col-2 text-center p-2" type="text" id="userZipcode" placeholder="Enter Zipcode.">
       <div class="col-1"></div>
-      <button class="col-2 text-center p-2" id="btn">Get Weather.</button>
+      <button class="col-2 text-center p-2 blue-btn" id="btn">Get Weather.</button>
     </div>
     <div id="weatherInfo" class="mt-5"</div>
 `);
@@ -46,27 +46,52 @@ function getWeatherData() {
       const tempC = (data.main.temp - 273.15).toFixed(1);
       const tempF = ((tempC * 9) / 5 + 32).toFixed(1);
 
+      const feelsLikeC = (data.main.feels_like - 273.15).toFixed(1);
+      const feelsLikeF = ((feelsLikeC * 9) / 5 + 32).toFixed(1);
+
       const weatherData = `
       <div class="row justify-content-center">
-        <div class="border border-dark text-center col-9"><b>City</b></div>
+        <div class="border border-dark text-center col-8 yellow-back"><b>City</b></div>
       </div>
       <div class="row justify-content-center">
-        <div class="border-bottom border-start border-end border-dark text-center col-9 p-3">${data.name}</div>
+        <div class="border-bottom border-start border-end border-dark text-center col-8 p-3">${data.name}</div>
       </div>
       <div class="row justify-content-center mt-5">
-        <div class="border border-dark text-center col-9"><b>Temperature</b></div>
+        <div class="border border-dark text-center col-8 yellow-back"><b>Temperature</b></div>
       </div>
       <div class="row justify-content-center">
-        <div class="col-3 text-center border-dark border-start border-bottom p-3">${data.main.temp}</div>
-        <div class="col-3 text-center border-dark border-start border-bottom p-3">${tempF} F</div>
-        <div class="col-3 text-center border-dark border-start border-bottom border-end p-3">${tempC} C</div>
+        <div class="col-2 text-center border-dark border-start border-bottom p-3">${data.main.temp} K</div>
+        <div class="col-2 text-center border-dark border-start border-bottom p-3">${tempF} F</div>
+        <div class="col-2 text-center border-dark border-start border-bottom border-end p-3">${tempC} C</div>
       </div>
       <div class="row justify-content-center mt-5">
-        <div class="border border-dark text-center col-9"><b>Condition</b></div>
+        <div class="border border-dark text-center col-8 yellow-back"><b>Condition</b></div>
       </div>
       <div class="row justify-content-center">
-        <div class="col-9 text-center border-dark border-bottom border-start border-end p-4">${data.weather[data.id].description}</div>
-      </div>`;
+        <div class="col-8 text-center border-dark border-bottom border-start border-end p-3">${data.weather[data.id].description}</div>
+      </div>
+      <div class="row justify-content-center mt-5">
+        <div class="border border-dark text-center col-8 yellow-back"><b>Feels like</b></div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-2 text-center border-dark border-start border-bottom p-3">${data.main.feels_like} K</div>
+        <div class="col-2 text-center border-dark border-start border-bottom p-3">${feelsLikeF} F</div>
+        <div class="col-2 text-center border-dark border-start border-bottom border-end p-3">${feelsLikeC} C</div>
+      </div>
+      <div class="row justify-content-center mt-5">
+        <div class="border border-dark text-center col-8 yellow-back"><b>Wind</b></div>
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-4 text-center border-dark border-bottom border-start border-end p-4">${data.wind.deg}°</div>
+        <div class="col-4 text-center border-dark border-bottom border-end p-4">${data.wind.speed}</div>
+      </div>
+      <div class="row justify-content-center mt-5">
+        <div class="border border-dark text-center col-8 yellow-back"><b>Other Information</b></div>
+      </div>
+      <div class="row justify-content-center mb-5">
+        <div class="col-8 text-center border-dark border-bottom border-start border-end p-4">${data.sys.country}</div>
+      </div>
+      `;
 
       weatherInfo.innerHTML = weatherData;
     })
